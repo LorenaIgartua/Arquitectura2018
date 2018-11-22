ce4dbe1d825c584f1384017a3ec86a42012066f6
Lorena Igartua <lorenaigartua@gmail.com>
2018-11-21 22:21:00 -0300

    docuwiki

---
+++ CACIC/conf/users.auth.php
@@ -0,0 +1,12 @@
+# users.auth.php
+# <?php exit()?>
+# Don't modify the lines above
+#
+# Userfile
+#
+# Format:
+#
+# login:passwordhash:Real Name:email:groups,comma,separated
+
+
+lorena:$1$RSYgYVCD$l6Iilq7ilMW2SIoaCGCSw.:LORENA:lorenaigartua@gmail.com:admin,user
